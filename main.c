2c8295a4e5808b1388dcaa930b9245deaae8eec4
Benoit Gallois <63228475+bgallois255@users.noreply.github.com>
2022-04-28 09:55:14 +0200

    led set when the robot detect an obstacle
    test values of IR sensors and IMU

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include <chprintf.h>
 #include <leds.h>
 #include <sensors/imu.h>
+#include <sensors/proximity.h>
 
 #include <detect_obstacle.h>
 
@@ -51,85 +52,30 @@ int main(void)
     //inits the motors
 	motors_init();
 
-	//init gyroscope
-	//imu_start();    Besoin d'un mutex pour l gyro aussi
-	//calibrate_gyro();
+
+
 
 	//float time;
-	int speedR=1000;
-	int speedL=1000;
+	int speedR=200;
+	int speedL=200;
 	int i=0;
-
+	int b;
 
 
 
     /* Infinite loop. */
     while (1) {
 
-    	int16_t a;
-    	a = get_acc_filtered(0, 20);
-
-    	chprintf((BaseSequentialStream *)&SDU1, a);
-
-
-    	/* PARTIE GYROSCOPE
-    	int j;
-    	int16_t res;
-    	while(j!=3)
-    	{
-    		res = get_gyro(j);
-    		if(res >= 1)
-    		{
-    			toggle_rgb_led(LED2, GREEN_LED, RGB_MAX_INTENSITY);
-    			toggle_rgb_led(LED4, GREEN_LED, RGB_MAX_INTENSITY);
-    			toggle_rgb_led(LED6, GREEN_LED, RGB_MAX_INTENSITY);
-    			toggle_rgb_led(LED8, GREEN_LED, RGB_MAX_INTENSITY);
-    		}
-    		j=j+1;
-    	}
-		*/
-
     	right_motor_set_speed(speedR);
     	left_motor_set_speed(speedL);
 
-    	//chprintf((BaseSequentialStream *)&SDU1, "Hello world");
-
-    	//CODE POUR LE CAPTEUR DE POSITION QUI FAIT RECULER LE ROBOT POUR LE CONTOURNER
-    	if(i>3 && i<10)  //si le capteur IR3 capte un objet alors les moteurs reculent et puis tourne sur eux meme et réavance pour éviter l'obstacle
-    		{
-    			speedR = 0;
-    			speedL = 575;
-
-    			while(i>3 && i<10)  // tant que le capteur capte l'objet il continue de reculer
-    			{
-    				led_set_if_obstacle(LED1, 1, 0, 0, 0);
-
-    				right_motor_set_speed(speedR);
-    				left_motor_set_speed(speedL);
-    				i = i+1;
-    			}
-    		}
 
 
-    	if(i>=12 && i<19)  //si le capteur IR3 capte un objet alors les moteurs reculent et puis tourne sur eux meme et réavance pour éviter l'obstacle
-    	    		{
-    	    			speedR = 575;
-    	    			speedL = 0;
-
-    	    			while(i>=10 && i<17)  // tant que le capteur capte l'objet il continue de reculer
-    	    			{
-    	    				led_set_if_obstacle(LED7, 0, 0, 0, 1);
-
-    	    				right_motor_set_speed(speedR);
-    	    				left_motor_set_speed(speedL);
-    	    				i = i+1;
-    	    			}
-    	    		}
+    	//chprintf((BaseSequentialStream *)&SDU1, "%d ", b);
 
 
-    	i = i+1;
-    	speedR = 1000;
-    	speedL = 1000;
+    	led_set_if_obstacle();
+
 
 
 
